ab422e01c43f1ebf45663343b17445b0b884a3bb
bbbb8264 <bbbb8264@gmail.com>
2015-03-13 20:33:00 +0800

    fibonacci loop version finish

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -12,9 +12,17 @@ fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
 	push {r3, r4, r5, lr}
-	
+	mov r5,r0
+	mov r3,#1
+	mov r4,#1
+loop:	mov r0,r3
+	mov r3,r4
+	add r4,r3,r0
+	sub r5,r5,#1
+	cmp r5,#0
+	bgt loop
 	pop {r3, r4, r5, pc}
-.loop:
 	
+
 	.size fibonacci, .-fibonacci
 	.end
